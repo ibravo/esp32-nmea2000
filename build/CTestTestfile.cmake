1cf171090df140055376f900fc21d3216c1f03aa
ibravo <ibravo@hotmail.com>
2024-08-03 10:30:12 -0400

    AtomS3 version

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ibravo/Programs/git/esp32-nmea2000/.pio/libdeps/m5stack-atom/ArduinoJson
+# Build directory: /home/ibravo/Programs/git/esp32-nmea2k/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
---
+++ build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ibravo/Programs/git/esp32-nmea2000/.pio/libdeps/m5stack-atom/ArduinoJson/src
+# Build directory: /home/ibravo/Programs/git/esp32-nmea2k/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
